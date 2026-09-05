d9485ddebd8188b553c9f643d4bf42bcbd231f65
Kaio <kaioinformatica7@gmail.com>
2026-09-05 19:33:48 -0300

    Tarefa

--- AcademiaDoZe.Console/Program.cs
+++ AcademiaDoZe.Console/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Kaio Fernandes Branco
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
